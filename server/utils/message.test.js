178abca65f8850023a7e25455d0599172d29d1d7
Timothy Recker <Timothy@Timothys-MBP.home>
2016-12-27 00:27:29 -0800

    added geolocation and modified indentation

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,14 +1,27 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMesage', () => {
-  it('should generate correct message object', () => {
-    var from = 'Jen';
-    var text = 'Some message';
-    var message = generateMessage(from, text);
+    it('should generate correct message object', () => {
+        var from = 'Jen';
+        var text = 'Some message';
+        var message = generateMessage(from, text);
 
-    expect(message.createdAt).toBeA('number');
-    expect(message).toInclude({from, text});
-  });
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from, text});
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var from = 'Tom';
+        var latitude = 1;
+        var longitude = 2;
+        var message = generateLocationMessage(from, latitude, longitude);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message.from).toBe('Tom');
+        expect(message.url).toBe('https://www.google.com/maps?q=1,2');
+    });
 });
